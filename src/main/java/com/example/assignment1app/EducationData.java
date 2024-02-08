c8cf12aaab58e7769170feba7cdb6bd838acfb2a
daniel montenegro <dfelipemont@gmail.com>
2024-02-07 22:02:53 -0500

    adding second stage and tableview

---
+++ src/main/java/com/example/assignment1app/EducationData.java
@@ -0,0 +1,34 @@
+package com.example.assignment1app;
+
+public class EducationData {
+    private final String year;
+    private final String revenue;
+
+    private final String employment;
+    private final String wages;
+
+
+    public EducationData(String year, String revenue, String employment, String wages) {
+        this.year = year;
+        this.revenue = revenue;
+        this.employment = employment;
+        this.wages = wages;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getRevenue() {
+        return revenue;
+    }
+
+    public String getEmployment() {
+        return employment;
+    }
+
+    public String getWages() {
+        return wages;
+    }
+
+}
--- src/main/java/com/example/assignment1app/ReportApplication.java
+++ src/main/java/com/example/assignment1app/ReportApplication.java
@@ -1,20 +1,26 @@
 package com.example.assignment1app;
-
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ReportApplication extends Application {
-
+    private final ObservableList<EducationData> data = FXCollections.observableArrayList();
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Report");
@@ -22,21 +28,102 @@ public class ReportApplication extends Application {
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("year");
         NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("revenue");
 
         LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.setTitle("Colleges and Universities in Canada Revenue");
+        lineChart.setTitle("Statistics of Colleges and Universities in Canada");
+        lineChart.setPadding(new Insets(25,25,0,25));
 
-        populateChart(lineChart);
+        GridPane gridPane = new GridPane();
 
-        Scene scene = new Scene(lineChart, 800 , 600);
+        final ToggleGroup group = new ToggleGroup();
+
+        RadioButton rb1 = new RadioButton("Revenue");
+        rb1.setToggleGroup(group);
+        rb1.setSelected(true);
+
+        RadioButton rb2 = new RadioButton("Employment");
+        rb2.setToggleGroup(group);
+
+        RadioButton rb3 = new RadioButton("Wages");
+        rb3.setToggleGroup(group);
+
+        Button button = new Button("Table View");
+
+        GridPane.setRowIndex(button, 0);
+        GridPane.setColumnIndex(button, 3);
+
+        GridPane.setRowIndex(rb1,0);
+        GridPane.setColumnIndex(rb1, 1);
+
+        GridPane.setRowIndex(rb2,1);
+        GridPane.setColumnIndex(rb2, 1);
+
+        GridPane.setRowIndex(rb3,2);
+        GridPane.setColumnIndex(rb3, 1);
+
+        GridPane.setMargin(rb1,new Insets(25,0 ,0,0));
+        GridPane.setMargin(button,new Insets(25,0 ,0,100));
+
+        gridPane.getChildren().addAll(rb1, rb2, rb3, button);
+
+        gridPane.setAlignment(Pos.CENTER);
+        populateChartRevenue(lineChart);
+
+        group.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            lineChart.getData().clear();
+            if (newValue == rb1) {
+                populateChartRevenue(lineChart);
+            } else if (newValue == rb2) {
+                populateChartEmployment(lineChart);
+            } else if (newValue == rb3) {
+                populateChartWages(lineChart);
+            }
+        });
+
+        VBox vbox = new VBox(lineChart,gridPane);
+        Scene scene = new Scene(vbox, 800 , 550);
+        scene.getStylesheets().add("file:src/main/java/com/example/assignment1app/styles.css");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
-    private void populateChart(LineChart<String, Number> lineChart) {
+        button.setOnAction(e->secondStage());
+
+    }//end of start method
+
+    private void secondStage() {
+        //        creating second stage
+        Stage secondStage = new Stage();
+        secondStage.setTitle("Table view");
+
+        TableView<EducationData> infoTable = new TableView<>();
+        TableColumn<EducationData, String> yearColumn = new TableColumn<>("year");
+        yearColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getYear()));
+
+        TableColumn<EducationData, String> revenueColumn = new TableColumn<>("revenue");
+        revenueColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRevenue()));
+
+        TableColumn<EducationData, String> employmentColumn = new TableColumn<>("employment");
+        employmentColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEmployment()));
+
+        TableColumn<EducationData, String> wagesColumn = new TableColumn<>("wages");
+        wagesColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getWages()));
+
+        infoTable.getColumns().addAll(yearColumn, revenueColumn, employmentColumn, wagesColumn);
+
+        showData(infoTable);
+
+        VBox vbox1 = new VBox(infoTable);
+        Scene scene1 = new Scene(vbox1, 800 , 550);
+        scene1.getStylesheets().add("file:src/main/java/com/example/assignment1app/styles.css");
+        secondStage.setScene(scene1);
+        secondStage.show();
+    }//end of secondStage method
+
+    private void populateChartRevenue(LineChart<String, Number> lineChart) {
+
         DatabaseConnector dbConnector = new DatabaseConnector();
         Connection connection = dbConnector.connect();
+
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT year, revenue FROM educationCanada");
@@ -55,15 +142,81 @@ public class ReportApplication extends Application {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
+    }//end of populateChartRevenue method
+
+    private void populateChartEmployment(LineChart<String, Number> lineChart) {
+        DatabaseConnector dbConnector = new DatabaseConnector();
+        Connection connection = dbConnector.connect();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT year, employment FROM educationCanada");
+
+            XYChart.Series<String, Number> series = new XYChart.Series<>();
+            series.setName("employment");
+
+            while (resultSet.next()) {
+                String year = resultSet.getString("year");
+                int employment = resultSet.getInt("employment");
+                series.getData().add(new XYChart.Data<>(year, employment));
+            }
+
+            lineChart.getData().add(series);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }//end of populateChartEmployment method
+
+    private void populateChartWages(LineChart<String, Number> lineChart) {
+        DatabaseConnector dbConnector = new DatabaseConnector();
+        Connection connection = dbConnector.connect();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT year, wages FROM educationCanada");
+
+            XYChart.Series<String, Number> series = new XYChart.Series<>();
+            series.setName("wages");
+
+            while (resultSet.next()) {
+                String year = resultSet.getString("year");
+                int wages = resultSet.getInt("wages");
+                series.getData().add(new XYChart.Data<>(year, wages));
+            }
+
+            lineChart.getData().add(series);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }//end of populateChartWages method
+
+    private void showData(TableView<EducationData> infoTable) {
+        DatabaseConnector dbConnector = new DatabaseConnector();
+
+
+
+        try (Connection connection = dbConnector.connect()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM educationCanada");
+            while (resultSet.next()) {
+                EducationData educationData = new EducationData(resultSet.getString("year"),
+                        resultSet.getString("revenue"), resultSet.getString("employment"),resultSet.getString("wages"));
+                data.add(educationData);
+            }
+            infoTable.setItems(data);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }//end of showDat method
+
 
 
     public static void main(String[] args) {
 
         launch();
     }
-
-
 
 
 }//end ReportApplication class
